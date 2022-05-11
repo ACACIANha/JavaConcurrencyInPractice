6ea957470d2e68541871009e72c542058f87feda
ACACIAN <gkehdrn95@netand.co.kr>
2022-05-11 10:21:50 +0900

    모니터 타이머 간단하게

--- src/main/java/concurrency/exercise/ch2/exercise3/Monitor.java
+++ src/main/java/concurrency/exercise/ch2/exercise3/Monitor.java
@@ -14,9 +14,18 @@ public class Monitor implements Runnable {
 	private final ConcurrentLinkedQueue< Person > personQueue;
 	private final List< Room > roomTasks;
 
+	private long elapseTime;
+	private long deltaTime;
+	private long prevTime;
+	private long currentTime;
+
 	private Monitor( ConcurrentLinkedQueue< Person > personQueue, List< Room > roomTasks ) {
 		this.personQueue = personQueue;
 		this.roomTasks = roomTasks;
+
+
+		this.prevTime = System.currentTimeMillis();
+		this.currentTime = System.currentTimeMillis();
 	}
 
 	public static Monitor newInstance( ConcurrentLinkedQueue< Person > personQueue, List< Room > roomTasks ) {
@@ -31,17 +40,26 @@ public class Monitor implements Runnable {
 
 		while ( hasWorkingTask() ) {
 
+			timeProcess();
 			log.info( "{}|persons: {}", personQueue.size(), personQueue );
 			StringBuilder temp = new StringBuilder();
 			roomTasks.forEach( room -> temp.append( room.toString() ).append( " " ) );
 			log.info( "{} ", temp );
 
-			int sleepTerm = 1;
+			int sleepTerm = 10;
 			TimeUnit.MILLISECONDS.sleep( 100 * sleepTerm );
 		}
 	}
 
-	public boolean hasWorkingTask() {
+	private void timeProcess() {
+		this.currentTime = System.currentTimeMillis();
+		this.deltaTime = this.currentTime - this.prevTime;
+		this.elapseTime += this.deltaTime;
+		log.info( "모니터 경과시간: {}", ( long ) ( this.elapseTime * 0.001 ) );
+		this.prevTime = this.currentTime;
+	}
+
+	private boolean hasWorkingTask() {
 
 		return !personQueue.isEmpty() || roomTasks.stream().anyMatch( Room::isWorking );
 	}
